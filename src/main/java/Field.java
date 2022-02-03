7d4222b4fe40eb1edb5780eef172e9edd142b5aa
justbegginer <46086383+justbegginer@users.noreply.github.com>
2022-02-03 16:38:18 +0300

    add field realisation

--- src/main/java/Field.java
+++ src/main/java/Field.java
@@ -1,2 +1,17 @@
 public class Field {
+    private int value;
+    public Field(){
+        this.value = 1; // default value means null field
+    }
+    public void increment(){
+        this.value *= 2;
+    }
+
+    public void makeNull(){
+        this.value = 1;
+    }
+
+    public int getValue() {
+        return this.value;
+    }
 }
