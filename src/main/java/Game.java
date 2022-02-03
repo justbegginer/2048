2b80aef7e7cd853b0f620e440d5666ec86c4ce1b
justbegginer <46086383+justbegginer@users.noreply.github.com>
2022-02-03 22:37:26 +0300

    make logic for Game and some prototypes of basic functions

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,2 +1,35 @@
 public class Game {
+    private Level level;
+    private int score;
+    public Game(int width){
+        this.level = new Level(width);
+        createRandom();
+        createRandom();
+    }
+    public void start(){
+        while(isDefeat()){
+        }
+    }
+    private void createRandom(){
+        this.level.incrementValue((int)(Math.random() * 1_000_000) % level.getLevelArray().size(),
+                (int)(Math.random() * 1_000_000) % level.getLevelArray().size());
+    }
+    private void draw(){
+        
+    }
+    private void moveRight(){
+
+    }
+    private void moveLeft(){
+
+    }
+    private void moveDown(){
+
+    }
+    private void moveUp(){
+
+    }
+    private boolean isDefeat(){
+        return false;
+    }
 }
