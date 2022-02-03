b7b9c537ac476e90c6e31d7e5a645e4385caffde
justbegginer <46086383+justbegginer@users.noreply.github.com>
2022-02-03 17:07:08 +0300

    add field realisation

--- src/main/java/Level.java
+++ src/main/java/Level.java
@@ -1,2 +1,22 @@
+import java.util.ArrayList;
+
 public class Level {
+    private ArrayList<ArrayList<Field>> levelArray;
+    public Level(int width){
+        this.levelArray = new ArrayList<>();
+        for (int i = 0; i < width; i++) {
+            this.levelArray.add(new ArrayList<>());
+            for (int j = 0; j < width; j++) {
+                this.levelArray.get(i).add(new Field());
+            }
+        }
+    }
+
+    public ArrayList<ArrayList<Field>> getLevelArray() {
+        return levelArray;
+    }
+
+    public void incrementValue(int i, int j){
+        levelArray.get(i).get(j).increment();
+    }
 }
